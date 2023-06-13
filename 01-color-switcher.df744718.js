eab78fd72b987438b1a456ded294c0c8de685d38
lastavchuk <126959996+lastavchuk@users.noreply.github.com>
2023-06-13 18:57:23 +0000

    Deploying to gh-pages from @ lastavchuk/js-09@3515f2999fb5f835c03fd9dc3adc2c0e3b1afacc 🚀

---
+++ 01-color-switcher.df744718.js
@@ -0,0 +1,2 @@
+const t={btnStart:document.querySelector("button[data-start]"),btnStop:document.querySelector("button[data-stop]"),body:document.querySelector("body")};let e;function n(){t.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}t.btnStop.disabled=!0,t.btnStart.addEventListener("click",(()=>{e=setInterval(n,1e3),t.btnStart.disabled=!0,t.btnStop.disabled=!1})),t.btnStop.addEventListener("click",(()=>{clearInterval(e),e=null,t.btnStart.disabled=!1,t.btnStop.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.df744718.js.map
